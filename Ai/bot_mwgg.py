b541edf903ad9ba0a475b1bfcc30ed7f37d7895a
Jonathan White <jon.white@dmapps.us>
2013-04-30 06:58:42 -0400

    Fix various libraries

--- Ai/bot_mwgg.py
+++ Ai/bot_mwgg.py
@@ -2,14 +2,14 @@
 #
 # This file is part of GoldenEye: Source's Python Library.
 #
-# GoldenEye: Source's Python Library is free software: you can redistribute 
-# it and/or modify it under the terms of the GNU General Public License as 
-# published by the Free Software Foundation, either version 3 of the License, 
+# GoldenEye: Source's Python Library is free software: you can redistribute
+# it and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the License,
 # or(at your option) any later version.
 #
-# GoldenEye: Source's Python Library is distributed in the hope that it will 
+# GoldenEye: Source's Python Library is distributed in the hope that it will
 # be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
 # Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
@@ -20,6 +20,7 @@ from GEAiConst import State
 from .bot_deathmatch import bot_deathmatch
 from .Schedules import BaseCondition, Sched, Cond
 from .Tasks import Task
+import GEGlobal as Glb
 import random
 
 USING_API = Glb.API_VERSION_1_1_0
